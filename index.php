38879991c02d2509b01f42a4ef74b32f92723de0
AdrienLapasset <aflapasset@gmail.com>
2022-06-28 11:13:47 +0200

    init

--- index.php
+++ index.php
@@ -5,16 +5,6 @@
     <h1>Pages :</h1>
     <div class="list-group">
       <a class="list-group-item list-group-item-action" href="home.php">Home</a>
-      <a class="list-group-item list-group-item-action" href="participer.php">Participer</a>
-      <a class="list-group-item list-group-item-action" href="accueil.php">Accueil</a>
-      <a class="list-group-item list-group-item-action" href="reinitialisation-mdp-1.php">Reinitialisation mdp 1</a>
-      <a class="list-group-item list-group-item-action" href="reinitialisation-mdp-2.php">Reinitialisation mdp 2</a>
-      <a class="list-group-item list-group-item-action" href="comment-participer.php">Comment participer</a>
-      <a class="list-group-item list-group-item-action" href="enregistrer-action.php">Enregistrer action</a>
-      <a class="list-group-item list-group-item-action" href="creation-compte.php">Création de compte</a>
-      <a class="list-group-item list-group-item-action" href="mes-coordonees.php">Mes coordonees</a>
-      <a class="list-group-item list-group-item-action" href="nouvelle-action.php">Nouvelle action</a>
-      <a class="list-group-item list-group-item-action" href="connexion.php">Connexion</a>
     </div>
   </div>
 </div>
