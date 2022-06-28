b0ae907cece5a04d65a431e48b3dc21cc245c6ad
AdrienLapasset <aflapasset@gmail.com>
2022-06-28 15:14:46 +0200

    home page

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 <main class="home">
     <div class="container">
         <div class="c-block">
-            <h1><b>Remplissez vite</b> le formulaire ci-dessous pour <b>tenter votre chance !</b></h1>
+            <h1><b>Remplissez vite</b> le formulaire ci-dessous <br>pour <b>tenter votre chance !</b></h1>
             <form action="">
                 <div class="row">
                     <div class="col-lg-6">
@@ -22,27 +22,55 @@
                         <input type="text" placeholder="Prénom*" required>
                     </div>
                     <div class="col-lg-6">
-                        <input type="text" placeholder="Nom*" required>
+                        <input type="text" placeholder="Adresse mail*" required>
                     </div>
                     <div class="col-lg-6">
-                        <input type="text" placeholder="Prénom*" required>
+                        <input type="text" placeholder="Numéro de téléphone*" required>
                     </div>
                     <div class="row">
                         <div class="col">
-                            <input type="text" placeholder="Prénom*" required>
+                            <input type="text" placeholder="Adresse postale*" required>
                         </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <input type="text" placeholder="Code postal*" required>
+                    </div>
+                    <div class="col-lg-6">
+                        <input type="text" placeholder="Ville*" required>
                     </div>
                     <div class="col-lg-6">
                         <input type="file" id="file">
                         <label for="file" class="input">
-                           <img class="me-2" src="assets/icons/photo.png" alt="">
-                        Parcourir
+                            <img class="me-2" src="assets/icons/photo.png" alt="">
+                            Photo du ticket de caisse*
                         </label>
                     </div>
                     <div class="col-lg-6">
-                        <input type="text" placeholder="Prénom*" required>
+                        <input type="file" id="file">
+                        <label for="file" class="input">
+                            <img class="me-2" src="assets/icons/photo.png" alt="">
+                            Photo du produit*
+                        </label>
                     </div>
+                    <div class="my-4">
+                      <div class="checkbox-container">
+                        <input type="checkbox" id="data" class="me-2">
+                        <label for="data">
+                          J’accepte que mes données personnelles** soient utilisées dans le cadre du jeu*
+                        </label>
+                      </div>
+                      <div class="checkbox-container">
+                        <input type="checkbox" id="data" class="me-2">
+                        <label for="data">
+                        J’ai lu et j’accepte le règlement du jeu*
+                        </label>
+                      </div>
+                    </div>
+                    <p class="info">
+                      ** Vos données sont collectées par la société Fromagerie Guilloteau pour la participation au jeu concours et la remise des lots. Le nom des gagnants ainsi que leur lieu d’habitation pourront également être utilisés dans des campagnes publicitaires liées au jeu. Vous disposez de droits sur vos données. Plus d’informations sur la gestion de vos données disponibles sur le règlement du jeu-concours disponible ici pavedaufinois.com
+                    </p>
                 </div>
+                <button type="submit" class="c-btn">Valider</button>
             </form>
         </div>
     </div>
--- partials/header.php
+++ partials/header.php
@@ -15,6 +15,6 @@
 </head>
 
 <body>
-  <header class="mb-4">
+  <header class="mb-5">
     <img class="d-block m-auto" src="assets/logos/LOGO PAVE D'AFFINOIS.png" alt="">
   </header>
