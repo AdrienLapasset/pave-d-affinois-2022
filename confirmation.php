298c6b1dd4d2307d55a2ed3f0b933446d4147e6b
AdrienLapasset <aflapasset@gmail.com>
2022-06-29 12:08:53 +0200

    add page confirmation

---
+++ confirmation.php
@@ -0,0 +1,15 @@
+<?php
+function includeHeader($page)
+{
+  include("partials/header.php");
+}
+includeHeader('confirmation')
+?>
+
+<main class="confirmation">
+    <div class="container">
+        <h1><b>Votre participation</b><br>a bien été prise en compte</h1>
+    </div>
+</main>
+
+<?php include 'partials/footer.php'; ?>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php include("partials/header.php"); ?>
 
+<main>
 <div class="container d-flex align-items-center justify-content-center py-5">
   <div class="w-100">
     <h1>Pages :</h1>
@@ -8,8 +9,10 @@
       <a class="list-group-item list-group-item-action" href="etape-1.php">Etape 1</a>
       <a class="list-group-item list-group-item-action" href="etape-2.php">Etape 2</a>
       <a class="list-group-item list-group-item-action" href="etape-3.php">Etape 3</a>
+      <a class="list-group-item list-group-item-action" href="confirmation.php">Confirmation</a>
     </div>
   </div>
 </div>
+</main>
 
 <?php include 'partials/footer.php'; ?>
