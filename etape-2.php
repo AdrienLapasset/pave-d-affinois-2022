85cd668d7ddee2be81b0594db3643cd1bb280004
AdrienLapasset <aflapasset@gmail.com>
2022-06-29 12:03:35 +0200

    add étapes

--- etape-2.php
+++ etape-2.php
@@ -11,6 +11,12 @@ includeHeader('etapes')
         <h1><b>Grattez</b><br> pour <b>tenter de remporter</b><br> votre panier <b>quitoque !</b></h1>
        <aside>Si vous découvrez un panier Quitoque, c’est gagné !</aside>
        <img src="assets/imgs/etape-2.png" alt="">
+       <div class="c-block mt-2">
+        <h2>Vous remportez votre panier Quitoque</h2>
+        <p>Un e-mail de confirmation de réception de votre dossier va vous être envoyé dans quelques instants. Nous reviendrons vers vous dès que vos preuves d’achats aurons été vérifiées et définitivement validées.
+Pensez à regarder dans vos courriers indésirables !</p>
+      </div>
+      <button class="c-btn">Suivant</button>
     </div>
 </main>
 
--- etape-3.php
+++ etape-3.php
@@ -10,7 +10,12 @@ includeHeader('etapes')
     <div class="container">
         <h1><b>Grattez</b><br> pour <b>tenter de remporter</b><br> votre panier <b>quitoque !</b></h1>
        <aside>Si vous découvrez un panier Quitoque, c’est gagné !</aside>
-       <img src="assets/imgs/etape-1.png" alt="">
+       <img src="assets/imgs/etape-3.png" alt="">
+       <div class="c-block mt-2">
+        <h2>Merci de votre participation</h2>
+        <p>N’hésitez pas à retenter votre chance<br> avec une nouvelle preuve d’achat !</p>
+      </div>
+      <button class="c-btn">Suivant</button>
     </div>
 </main>
 
