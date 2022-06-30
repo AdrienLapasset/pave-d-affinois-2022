adcc50b9968ff595463ae362b76164820da9b557
AdrienLapasset <aflapasset@gmail.com>
2022-06-30 11:20:43 +0200

    fix background reponsive

--- confirmation.php
+++ confirmation.php
@@ -7,9 +7,9 @@ includeHeader('bg-confirmation')
 ?>
 
 <main class="confirmation">
-  <div class="container">
-    <h1><b>Votre participation</b><br>a bien été prise en compte</h1>
-  </div>
+  <h1><b>Votre participation</b><br>a bien été prise en compte</h1>
+  <img class="img-fluid d-none d-sm-block" src="assets/imgs/confirmation.png" alt="">
+  <img class="img-fluid d-sm-none" src="assets/imgs/original.png" alt="">
 </main>
 
 <?php include 'partials/footer.php'; ?>
